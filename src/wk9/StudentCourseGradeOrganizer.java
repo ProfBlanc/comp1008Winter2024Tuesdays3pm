55c7c7cd20c46bd420ec10d4779491780ee46a1e
Prof Blanc <ben.blanc@georgebrown.ca>
2024-03-12 21:16:36 -0400

    Class Code Mar 12, 2024 @ 17h00

--- src/wk9/StudentCourseGradeOrganizer.java
+++ src/wk9/StudentCourseGradeOrganizer.java
@@ -3,36 +3,128 @@ package wk9;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class StudentCourseGradeOrganizer {
     static Scanner input = new Scanner(System.in);
-    static Path path = Path.of("src/wk9");;
+    static Path path = Path.of("src/wk9");
     public static void main(String[] args) {
-        welcome();
         try {
-            getInfo();
+            welcome();
         }
         catch (Exception e){
             System.err.println(e);
         }
     }
-    static void welcome(){
+    static void welcome() throws Exception{
         System.out.println("Welcome to our Student Course Grade Organizer");
+        ArrayList<String> options = new ArrayList<>();
+        options.add("add");
+        options.add("delete");
+        options.add("update");
+        options.add("view");
+
+        String message = "What would you like to do? Enter one " +
+                "of the following options: ";
+        for(String option : options){
+            message += option + ", ";
+        }
+        message = message.substring(0, message.length() - 2);
+
+        System.out.println(message);
+        String choice = input.nextLine().toLowerCase();
+
+        if(options.contains(choice)){
+            switch (choice){
+                case "add":
+                    getInfo();
+                    break;
+                case "delete":
+                    delete();
+                    break;
+                case "update":
+                    update();
+                    break;
+                default:
+                    view();
+                    break;
+            }
+        }
+    }
+    static void view() throws Exception{
+        String year = askQuestion(1);
+        String semester = askQuestion(2);
+        String courseCode = askQuestion(3);
+        String evaluation = askQuestion(4);
+        path = path.resolve(year)
+                .resolve(semester)
+                .resolve(courseCode)
+                .resolve(evaluation + ".txt");
+        if(Files.exists(path)){
+            System.out.println("Grade for " + evaluation
+                    + " of " +courseCode + " in " + year
+                    + " " + semester + " is: " + Files.readString(path));
+
+        }
+
+    }
+    static void delete() throws Exception{
+        String year = askQuestion(1);
+        String semester = askQuestion(2);
+        String courseCode = askQuestion(3);
+        String evaluation = askQuestion(4);
+
+        path = path.resolve(year)
+                .resolve(semester)
+                .resolve(courseCode)
+                .resolve(evaluation + ".txt");
+        if(Files.exists(path)){
+            Files.delete(path);
+            System.out.println("Deletion successful");
+        }
+        else{
+            System.out.println("Could not delete");
+        }
+
+    }
+    static void update() throws Exception{
+        String year = askQuestion(1);
+        String semester = askQuestion(2);
+        String courseCode = askQuestion(3);
+        String evaluation = askQuestion(4);
+        path = path.resolve(year)
+                .resolve(semester)
+                .resolve(courseCode)
+                .resolve(evaluation + ".txt");
+
+        System.out.println("Enter new grade");
+        String grade = input.nextLine();
+        Files.writeString(path, grade);
+
+    }
+
+    static String askQuestion(int number){
+        String message = switch (number){
+            case 1-> "Enter a year";
+            case 2-> "Enter a semester";
+            case 3-> "Enter course code or type quit to exit";
+            default -> "Enter an evaluation";
+
+        };
+        System.out.println(message);
+        return input.nextLine();
     }
     static void getInfo() throws Exception{
-        System.out.println("Enter the year");
-        String year = input.nextLine();
-        System.out.println("Enter the semester");
-        String semester = input.nextLine();
+        String year = askQuestion(1);
+        String semester = askQuestion(2);
         path = path.resolve(year).resolve(semester);
         if(!Files.exists(path)){
             Files.createDirectories(path);
         }
 
         while(true){
-            System.out.println("Enter a course code or quit to exit");
-            String courseCode = input.nextLine();
+            String courseCode = askQuestion(3);
             if(courseCode.trim().length() == 0 || courseCode.toLowerCase().charAt(0) == 'q')
                 break;
 
